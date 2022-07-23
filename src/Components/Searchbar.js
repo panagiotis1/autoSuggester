6628cbf8d1dc3e5457cde4fe46f831c779922842
Panos Bakavos <panbakavos@gmail.com>
2022-07-23 13:28:38 +0300

    debounce added

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,70 +1,71 @@
 import React from "react"
 import "./Searchbar.css"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import SearchIcon from '@mui/icons-material/Search'
-import _ from "lodash";
 import { fromFetch } from 'rxjs/fetch';
-import { switchMap, of, catchError  } from 'rxjs';
+import { switchMap, of, catchError, debounce, interval  } from 'rxjs';
 import { fromEvent } from 'rxjs';
 
 export default function Searchbar() {
-    const [state, setState] = useState({loading: false, hits: [], input: ""})
+    const [state, setState] = useState({loading: false, hits: [], input: "", error: {value: false, message: ""}})
+    const elemRef = useRef(null);
     useEffect(() => {
-        // TODO: replace id first parameter of fromEvent with a useref variable
-        fromEvent(document.getElementById("testing"), "input")
+        fromEvent(elemRef.current, "input")
+            .pipe(debounce(()=>interval(200)))
             .pipe(switchMap((e) => {
-                let url = new URL('https://hn.algolia.com/api/v1/search')
-                url.searchParams.append("query", e.target.value)
-                const $data = fromFetch(url).pipe(
-                    switchMap((response) => {
-                        if (response.ok) {
-                            // OK return data
-                            return response.json();
-                        } else {
-                            // Server is returning a status requiring the client to try something else.
-                            return of({ error: true, message: `Error ${ response.status }` });
-                        }
-                    }),
-                    catchError(err => {
-                        // Network or other error, handle appropriately
-                        console.error(err);
-                        return of({ error: true, message: err.message })
-                    })
-                )
-                return $data
-            }
-        )).subscribe({
+                if (e.target.value.length >= 3) {
+                    setState((state) => ({ ...state, loading: true,  input: e.target.value}))
+                    let url = new URL('https://hn.algolia.com/api/v1/search')
+                    url.searchParams.append("query", e.target.value)
+                    const $data = fromFetch(url).pipe(
+                        switchMap((response) => {
+                            if (response.ok) {
+                                // OK return data
+                                return response.json();
+                            } else {
+                                // Server is returning a status requiring the client to try something else.
+                                return of({ error: true, message: `Error ${ response.status }` });
+                            }
+                        }),
+                        catchError(err => {
+                            // Network or other error, handle appropriately
+                            console.error(err);
+                            return of({ error: true, message: err.message })
+                        })
+                    )
+                    return $data
+
+                } else {
+                    return of({hits:[]})
+                }
+        })).subscribe({
             next: results => {
-                // results.then((x)=> console.log("hey", x))
-                // console.log("resulsts---> ", results)
-                setState(state => ({ ...state, loading: false, hits: results.hits }))
+                setState(state => ({
+                    ...state,
+                    loading: false,
+                    hits: results.hits || [],
+                    error: {
+                        value: results.error,
+                        message: results.message || ""
+                    }
+                }))
             },
-            error:err=>{
+            error: err => {
                 console.error(err);
             },
             complete: () => console.log('done')
-        });;
+        });
+        return () => console.log("clean up function"); 
     }, []);
-
-    function handleChange(event) {
-        // move this code and make sure it works.
-        if (event.target.value.length >= 3) {
-            setState((state) => ({ ...state, loading: true,  input: event.target.value}))
-            // here we were handling the requests...
-        } else {
-            setState(x => ({ ...x, hits: [] }))
-        }
-    }
+    
 
     return (
         <div className="search">
             <div className="searchInput">
-                {/* TODO: remove id & handlechange. Move debounce wherever must be */}
                 <input
-                    id="testing"
+                    ref={elemRef}
                     type="text"
                     placeholder={"Search title"}
-                    onChange={_.debounce( handleChange, 500)} 
                 />
 
                 {state.hits.length === 0 && !state.loading && <div className="searchIcon"><SearchIcon /></div>}
@@ -93,6 +94,7 @@ export default function Searchbar() {
                 </div>
             )}
             {state.loading && <div>Loading...</div>}
+            {state.error.value && <div>{state.error.message}</div>}
         </div>
     )
 }
